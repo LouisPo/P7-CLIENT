9e1c8baa09a0325e55e6f585bf676c0c6d514f8b
LouisPo <louispoirot38@gmail.com>
2022-12-18 19:55:48 +0100

    finalisation objectifs ticket1

--- src/main/java/com/oc/p7v2client/proxies/BorrowProxy.java
+++ src/main/java/com/oc/p7v2client/proxies/BorrowProxy.java
@@ -1,7 +1,6 @@
 package com.oc.p7v2client.proxies;
 
 import com.oc.p7v2client.beans.BorrowBean;
-import com.oc.p7v2client.beans.UserBean;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +9,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name="P7-V2-API",url="localhost:8083")
+@FeignClient(name="P10-API",url="localhost:8083")
 public interface BorrowProxy {
     @GetMapping(value = "users/account/borrows")
     public List<BorrowBean> borrowList(@CookieValue(value ="jwtToken")String token);
     @PostMapping(value="/users/account/borrows/extend")
-    public void extendABorrow(@RequestParam Integer borrowId);
+    public BorrowBean extendABorrow(@RequestParam Integer borrowId);
+
+
 }
--- src/main/java/com/oc/p7v2client/proxies/UserProxy.java
+++ src/main/java/com/oc/p7v2client/proxies/UserProxy.java
@@ -1,17 +1,18 @@
 package com.oc.p7v2client.proxies;
 
-import com.oc.p7v2client.beans.BookSlimBean;
+import com.oc.p7v2client.beans.BorrowBean;
+import com.oc.p7v2client.beans.ReservationBean;
 import com.oc.p7v2client.beans.UserBean;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import java.util.List;
 
-@FeignClient(name="P7-V2-API",url="localhost:8083")
+@FeignClient(name="P10-API",url="localhost:8083")
 public interface UserProxy {
     @PostMapping(value="/login")
     public String authenticate(@RequestParam(value="username") String username, @RequestParam(value="password") String password);
@@ -19,4 +20,12 @@ public interface UserProxy {
     @GetMapping(value="/users/account")
     public UserBean getUserFromToken(@CookieValue(value ="jwtToken")String token);
 
+    @GetMapping(value = "users/borrowsFromUserId")
+    public List<BorrowBean> findBorrowsByUserId(@RequestParam Integer userId);
+
+    @GetMapping(value = "/users/account/reservations")
+    List<ReservationBean> reservationList(@CookieValue(value ="jwtToken")String token);
+
+    @PostMapping("/users/account/reservations/delete")
+    public ResponseEntity deleteAReservation(@RequestParam Integer reservationId);
 }
